951599734300c14b12e09dc80706e46613cee458
oxxide216 <vie.devr@gmail.com>
2025-09-22 12:56:54 +0300

    Simplified text editor example

--- tests/text-editor/main.c
+++ tests/text-editor/main.c
@@ -10,44 +10,19 @@
 #define MAIN_BG_COLOR vec4(0.1, 0.1, 0.1, 0.8)
 #define TEXT_COLOR    vec4(0.5, 0.5, 0.7, 1.0)
 #define TEXT_SIZE     36.0
-#define SCROLL_SPEED  15.0
+#define SCROLL_SPEED  vec2(15, 15.0)
 
-typedef enum {
-  ActionNone = 0,
-  ActionQuit,
-  ActionSave,
-  ActionOpen,
-} Action;
-
-Action process_event(WinxEvent *event, bool *is_ctrl_pressed, f32 *scroll) {
+bool process_event(WinxEvent *event) {
   if (event->kind == WinxEventKindQuit) {
-    return ActionQuit;
+    return false;
   } else if (event->kind == WinxEventKindResize) {
     u32 width = event->as.resize.width;
     u32 height = event->as.resize.height;
 
     glass_resize(width, height);
-  } else if (event->kind == WinxEventKindKeyPress) {
-    if (event->as.key.key_code == WinxKeyCodeLeftControl)
-      *is_ctrl_pressed = true;
-    else if (event->as.key.key_code == WinxKeyCodeS && *is_ctrl_pressed)
-      return ActionSave;
-    else if (event->as.key.key_code == WinxKeyCodeO && *is_ctrl_pressed)
-      return ActionOpen;
-  } else if (event->kind == WinxEventKindKeyRelease) {
-    if (event->as.key.key_code == WinxKeyCodeLeftControl)
-      *is_ctrl_pressed = false;
-  } else if (event->kind == WinxEventKindButtonPress) {
-    if (event->as.button.button == WinxMouseButtonWheelUp)
-      *scroll -= SCROLL_SPEED;
-    else if (event->as.button.button == WinxMouseButtonWheelDown)
-      *scroll += SCROLL_SPEED;
-
-    if (*scroll < 0.0)
-      *scroll = 1.0;
   }
 
-  return ActionNone;
+  return true;
 }
 
 void setup_styles(Glui *glui) {
@@ -57,10 +32,10 @@ void setup_styles(Glui *glui) {
   glui_get_style(glui, "text-editor")->fg_color = TEXT_COLOR;
 }
 
-void render_ui(Glui *glui, GluiTextEditor **editor, f32 scroll) {
+void render_ui(Glui *glui, GluiTextEditor **editor) {
   glui_begin_list(glui, GluiListKindHorizontal, vec2(5.0, 5.0), "block");
 
-  *editor = glui_text_editor(glui, TEXT_SIZE, vec2(0.0, scroll), "text-editor");
+  *editor = glui_text_editor(glui, TEXT_SIZE, SCROLL_SPEED, "text-editor");
 
   glui_end_list(glui);
 }
@@ -74,58 +49,17 @@ int main(void) {
   Glui glui = glui_init(&window, "tests/JetBrainsMono-Regular.ttf");
   setup_styles(&glui);
 
-  GluiTextEditor *editor = NULL;;
-  bool is_ctrl_pressed = false;
-  f32 scroll = 0.0;
+  GluiTextEditor *editor = NULL;
   bool is_running = true;
 
   while (is_running) {
     for (u32 i = 0; i < glui.events.len; ++i) {
-      Action action = process_event(glui.events.items + i,
-                                    &is_ctrl_pressed,
-                                    &scroll);
-
-      if (action == ActionQuit) {
-        is_running = false;
+      is_running = process_event(glui.events.items + i);
+      if (!is_running)
         break;
-      } else if (action == ActionSave && editor) {
-        StringBuilder sb = {0};
-        for (u32 i = 0; i < editor->lines.len; ++i) {
-          if (i > 0)
-            sb_push_char(&sb, '\n');
-
-          Str line_str = {
-            malloc(editor->lines.items[i].len),
-            editor->lines.items[i].len,
-          };
-
-          for (u32 j = 0; j < editor->lines.items[i].len; ++j) {
-            line_str.ptr[j] = editor->lines.items[i].items[j];
-          }
-
-          sb_push_str(&sb, line_str);
-        }
-
-        write_file("text.txt", sb_to_str(sb));
-      } else if (action == ActionOpen && editor) {
-        editor->lines.len = 1;
-        GluiTextEditorLine *line = editor->lines.items;
-        line->len = 0;
-
-        Str content = read_file("text.txt");
-        for (u32 j = 0; j < content.len; ++j) {
-          if (content.ptr[j] == '\n') {
-            DA_APPEND(editor->lines, (GluiTextEditorLine) {0});
-            ++line;
-            continue;
-          }
-
-          DA_APPEND(*line, (GluiWChar) content.ptr[j]);
-        }
-      }
     }
 
-    render_ui(&glui, &editor, scroll);
+    render_ui(&glui, &editor);
     glui_next_frame(&glui);
     winx_draw(&window);
   }
